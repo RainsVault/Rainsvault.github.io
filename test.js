e336718d7ddea1d45181944a6832855a71bc4f22
Rain <114886093+RainsVault@users.noreply.github.com>
2022-12-19 22:33:52 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,20 +1,4 @@
-function get_cookies_array() {
-
-var cookies = { };
-
-if (document.cookie && document.cookie != ”) {
-var split = document.cookie.split(‘;’);
-for (var i = 0; i < split.length; i++) {
-var name_value = split[i].split(“=”);
-name_value[0] = name_value[0].replace(/^ /, ”);
-cookies[decodeURIComponent(name_value[0])] = decodeURIComponent(name_value[1]);
-}
-}
-
-return cookies;
-
-}
-var cookies = get_cookies_array();
-for(var name in cookies) {
-console.log( name + ” : ” + cookies[name] + “
-” );
+document.cookie.split(';').reduce((cookies, cookie) => {
+  const [ name, value ] = cookie.split('=').map(c => c.trim());
+  return { ...cookies, [name]: value };
+}, {});
